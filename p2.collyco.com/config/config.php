6f342e9bd3d5ada0bc2b35f0a1cf747f0b3e1fd4
Curtis Colly <curtiscolly@yahoo.com>
2012-10-25 21:48:59 -0400

    Just changing the config.php for error messages

--- p2.collyco.com/config/config.php
+++ p2.collyco.com/config/config.php
@@ -19,7 +19,7 @@ If you want a different default time format for this app, set it below
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
-	define('APP_EMAIL', 'webmaster@myapp.com'); 
+	define('APP_EMAIL', 'curtiscolly@yahoo.com'); 
 
 /* 
 A email designated to receive messages from the server. Examples:
@@ -43,4 +43,4 @@ A email designated to receive messages from the server. Examples:
 
 # For extra security, you might want to set different salts than what the core uses
 	//define('PASSWORD_SALT', 'icosidodecahedron');
-	//define('TOKEN_SALT', '');
+	//define('TOKEN_SALT', 'whambamthankyoumam');
