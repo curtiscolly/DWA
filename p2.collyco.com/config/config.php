df36403c67a3f2af0ed1afb159785836af084c42
Curtis Colly <curtiscolly@yahoo.com>
2012-10-25 22:52:27 -0400

    update of the config.php file
    This will hopefully make it so that I can see what is wrong with my
    database on the live server

--- p2.collyco.com/config/config.php
+++ p2.collyco.com/config/config.php
@@ -29,7 +29,7 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'curtiscolly@yahoo.com'); 
 
 # Default DB name for this app
 	define('DB_NAME', "collycoc_p2_collyco_com"); 
